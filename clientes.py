31a0a79fd2c5fbb89529ab4fb23905702c4f70f0
Pedro Bellotti <bellottipcpedro@gmail.com>
2021-08-21 16:02:32 -0300

    Melhora mensagens de erro e adiciona delete

--- clientes.py
+++ clientes.py
@@ -1,5 +1,4 @@
 from flask_restful import Resource, marshal_with, reqparse, fields
-from flask import request
 from banco import banco, ClienteModelo, ClienteSchema
 
 schemaClientes = ClienteSchema(many=True) #Varios clientes
@@ -15,35 +14,46 @@ campos = {
 
 class Clientes(Resource):
 
-    #GET - lista todos clientes
-    def get(self):
-        todosClientes = ClienteModelo.query.all()
-        clientes = schemaClientes.dump(todosClientes)
-        return {'status': 'success', 'data': clientes}, 200
-
-    #POST - Cria um cliente
-    @marshal_with(campos)
-    def post(self):
-        #Recebe os dados
+    #Le os dados
+    def parse(self):
         parse = reqparse.RequestParser()
         parse.add_argument("codigo", type=int, help="Codigo do cliente", required=True)
         parse.add_argument("nome", type=str, help="Nome do cliente", required=True)
         parse.add_argument("razao_social", type=str, help="Razao social do cliente", required=True)
         parse.add_argument("cnpj", type=str, help="CNPJ do cliente", required=True)
         args = parse.parse_args()
+        return args
+
+    #GET - lista todos clientes
+    def get(self):
+        try:
+            todosClientes = ClienteModelo.query.all()
+            clientes = schemaClientes.dump(todosClientes)
+            return {'status': '200', 'data': clientes}, 200
+        except:
+            return {'status': '204', 'message': 'Algum erro aconteceu', 'error': 'Erro na listagem dos clientes'}, 204
+
+    #POST - Cria um cliente
+    @marshal_with(campos)
+    def post(self):
+        #Recebe os dados
+        args = self.parse()
         if not args:
-            return {'error': 'Sem dados'}, 400
+            return {'status': '400', 'message': 'Algum erro aconteceu', 'error': 'Sem dados'}, 400
         result = ClienteModelo.query.filter_by(codigo=args['codigo']).first()
         if result:
-            return {'error': 'Cliente com código já existente'}, 400
-        cliente = ClienteModelo(codigo=args['codigo'],
-            nome=args['nome'],
-            razao_social=args['razao_social'],
-            cnpj=args['cnpj']
-            )
-        banco.session.add(cliente)
-        banco.session.commit()
-        return cliente, 201
+            return {'status': '400', 'message': 'Algum erro aconteceu', 'error': 'Cliente com código já existente'}, 400
+        try:
+            cliente = ClienteModelo(codigo=args['codigo'],
+                nome=args['nome'],
+                razao_social=args['razao_social'],
+                cnpj=args['cnpj']
+                )
+            banco.session.add(cliente)
+            banco.session.commit()
+            return {'status': '201', 'message': 'Cliente criado com sucesso', 'error': 'null'}, 201
+        except:
+            return {'status': '400', 'message': 'Algum erro aconteceu', 'error': 'Erro na criação do cliente'}, 400
 
     #PUT - Edita um cliente
     def put(self):
@@ -51,4 +61,16 @@ class Clientes(Resource):
 
     #DELETE - Remove um cliente
     def delete(self):
-        return {"clientes": "DELETE"}
+        #Recebe os dados
+        args = self.parse()
+        if not args:
+            return {'status': '400', 'message': 'Algum erro aconteceu', 'error': 'Sem dados'}, 400
+        result = ClienteModelo.query.filter_by(codigo=args['codigo']).first()
+        if not result:
+            return {'status': '422', 'message': 'Algum erro aconteceu', 'error': 'Cliente não existe'}, 422
+        try:
+            ClienteModelo.query.filter_by(codigo=args['codigo']).delete()
+            banco.session.commit()
+            return {'status': '200', 'message': 'Cliente removido com sucesso', 'error': 'null'}, 200
+        except:
+            return {'status': '400', 'message': 'Algum erro aconteceu', 'error': 'Erro na criação do cliente'}, 400
--- testes.py
+++ testes.py
@@ -3,18 +3,18 @@ import requests
 url = 'http://127.0.0.1:5000/api/'
 
 #Adiciona clientes
-cliente={'codigo':'321',
+cliente1={'codigo':'321',
 'nome':'Teste 1', 
 'razao_social': 'Cliente de Teste',
 'cnpj': '12.345.678/0001-10'}
-resp = requests.post(url + 'clientes', data=cliente)
+resp = requests.post(url + 'clientes', data=cliente1)
 print(resp.json())
 
-cliente={'codigo':'987',
+cliente2={'codigo':'987',
 'nome':'Teste 2', 
 'razao_social': 'Cliente de Teste 2',
 'cnpj': '98.765.432/0001-99'}
-resp = requests.post(url + 'clientes', data=cliente)
+resp = requests.post(url + 'clientes', data=cliente2)
 print(resp.json())
 
 #Lista todos os clientes
@@ -22,5 +22,7 @@ resp = requests.get(url + 'clientes')
 print(resp.json())
 
 #Remove o cliente codigo 321
+resp = requests.delete(url + 'clientes', data=cliente1)
+print(resp.json())
 
 #Atualiza o nome do cliente 987
